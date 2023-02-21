25b315f20a6866bac0c2a1ced93ca06b4cb3a649
memoTux <romeo@mendezfuentes.net>
2023-02-20 22:50:05 -0600

    add pages

---
+++ pages/contacts.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: Contacts
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/team.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: Team
+  </div>
+</template>
+
+<style scoped></style>
