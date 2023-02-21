f5732709ab8b62738d3a713148c24382307311cd
memoTux <romeo@mendezfuentes.net>
2023-02-20 22:47:25 -0600

    add top & side bar items

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,7 +1,46 @@
+<script setup lang="ts">
+const items = [
+  { title: 'Dashboard', icon: 'mdi:mdi-view-dashboard', to: '/' },
+  { title: 'Data', type: 'subheader' },
+  { title: 'Manage Team', icon: 'mdi:mdi-account-group', to: '/team' },
+  { title: 'Contacts', icon: 'mdi:mdi-contacts', to: '/contacts' },
+  { title: 'Invoices Balances', icon: 'mdi:mdi-receipt', to: '/invoices' },
+  { title: 'Pages', type: 'subheader' },
+  { title: 'Profile Form', icon: 'mdi:mdi-account', to: '/form' },
+  { title: 'Calendar', icon: 'mdi:mdi-calendar-today', to: '/calendar' },
+  { title: 'FAQ Page', icon: 'mdi:mdi-frequently-asked-questions', to: '/faq' },
+  { title: 'Charts', type: 'subheader' },
+  { title: 'Bar Chart', icon: 'mdi:mdi-chart-bar', to: '/bar' },
+  { title: 'Pie Chart', icon: 'mdi:mdi-chart-pie', to: '/pie' },
+  { title: 'Line Chart', icon: 'mdi:mdi-chart-line', to: '/line' },
+  { title: 'Geography Chart', icon: 'mdi:mdi-map', to: '/geo' },
+]
+</script>
+
 <template>
-  <v-navigation-drawer>
+  <v-navigation-drawer color="#1F2A40">
     <v-list>
-      <v-list-item title="Navigation drawer"></v-list-item>
+      <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.png" title="John Leider"
+        subtitle="john@google.com">
+        <template v-slot:append>
+          <v-btn size="small" variant="text" icon="mdi:mdi-menu-down"></v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list :lines="false" nav>
+      <template v-for="(item) in items" :key="item.title">
+        <v-list-subheader v-if="item.type === 'subheader'" :title="item.title" />
+        <v-list-item v-else active-color="secondary" :to="item.to">
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item>
+      </template>
     </v-list>
   </v-navigation-drawer>
 </template>
--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,4 +1,25 @@
+<script setup lang="ts">
+import { useTheme } from 'vuetify';
+
+const theme = useTheme()
+const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+</script>
+
 <template>
-  <v-app-bar title="Application bar">
+  <v-app-bar color="#1F2A40">
+    <v-container fluid>
+      <v-row justify="space-between">
+        <v-col cols="3">
+          <v-text-field density="compact" variant="plain" label="Search" append-inner-icon="mdi:mdi-magnify" single-line
+            hide-details />
+        </v-col>
+        <v-col cols="3" class="text-right">
+          <v-btn icon="mdi:mdi-theme-light-dark" @click="toggleTheme" />
+          <v-btn icon="mdi:mdi-bell-outline" />
+          <v-btn icon="mdi:mdi-cog-outline" />
+          <v-btn icon="mdi:mdi-account" />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app-bar>
 </template>
