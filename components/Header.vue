e64c0d1f2b72294872b02c0f42ce7df0957a10e9
memoTux <romeo@mendezfuentes.net>
2023-02-23 20:31:33 -0600

    add Page Header Component

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ title: string, subtitle?: string }>()
+</script>
+
+<template>
+  <v-container>
+    <h2>{{ title }}</h2>
+    <p v-if="subtitle">{{ subtitle }}</p>
+  </v-container>
+</template>
