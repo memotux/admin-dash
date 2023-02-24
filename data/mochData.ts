85fa87a7d720564c0fcee40e9d1b4296315af669
memoTux <romeo@mendezfuentes.net>
2023-02-24 07:17:43 -0600

    update mochData to use colorTokens

--- data/mochData.ts
+++ data/mochData.ts
@@ -1,4 +1,4 @@
-import { tokens } from "../theme";
+import { colorTokens as tokens } from "~~/composables/ui";
 
 export const mockDataTeam = [
   {
@@ -433,7 +433,7 @@ export const mockPieData = [
 export const mockLineData = [
   {
     id: "japan",
-    color: tokens("dark").greenAccent[500],
+    color: tokens.dark.greenAccent[500],
     data: [
       {
         x: "plane",
@@ -487,7 +487,7 @@ export const mockLineData = [
   },
   {
     id: "france",
-    color: tokens("dark").blueAccent[300],
+    color: tokens.dark.blueAccent[300],
     data: [
       {
         x: "plane",
@@ -541,7 +541,7 @@ export const mockLineData = [
   },
   {
     id: "us",
-    color: tokens("dark").redAccent[200],
+    color: tokens.dark.redAccent[200],
     data: [
       {
         x: "plane",
