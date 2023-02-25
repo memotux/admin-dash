718412655b4e463dbf8b3804de2fe14f38f251b6
memoTux <romeo@mendezfuentes.net>
2023-02-25 10:05:42 -0600

    add form page content & composable

--- composables/ui.ts
+++ composables/ui.ts
@@ -1,9 +1,35 @@
-export const useUi = () => useState('ui', () => {
-  return {
-    drawer: true,
-    rail: false,
-  }
-})
+import { z } from 'zod'
+
+export const useUi = () => useState('ui', () => ({
+  drawer: true,
+  rail: false,
+}))
+
+export const useContactAction = () => useState('contactAction', () => new Set<number>())
+
+export const UserSchema = {
+  firstName: z.string()
+    .min(2, { message: 'Nombre debe contener tres o mas caracteres.' })
+    .refine((val) => !/[0-9]/.test(val), { message: 'Nombre NO debe contener numeros.' }),
+  lastName: z.string()
+    .min(2, { message: 'Apellido debe contener tres o mas caracteres.' })
+    .refine((val) => !/[0-9]/.test(val), { message: 'Apellido NO debe contener numeros.' }),
+  email: z.string().email('Correo debe ser valido.'),
+  contact: z.string()
+    // .min(11, { message: 'Telefono debe contener once o mas caracteres.' })
+    .refine((val) =>
+      !/^((\+[503|1][ -]?)|(\([503|1]\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/.test(val), { message: 'Telefono no valido.' }),
+  address: z.string()
+    .min(3, { message: 'Direccion debe contener tres o mas caracteres.' })
+}
+
+export const useFormState = () => useState('form', () => ({
+  firstName: '',
+  lastName: '',
+  email: '',
+  contact: '',
+  address: ''
+}))
 
 export const colorTokens = {
   dark: {
--- pages/form.vue
+++ pages/form.vue
@@ -1,9 +1,65 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const formState = useFormState()
+
+const handleSubmit = async (event: any) => {
+  const result = await event
+  if (result.valid) {
+    console.log(formState.value)
+  }
+}
+
+const fieldTest = (val: any, field: keyof typeof UserSchema) => {
+  const res = UserSchema[field].safeParse(val)
+  return res.success ? true : res.error.errors[0].message
+}
+
+const fieldRules = {
+  firstName: (val: any) => fieldTest(val, 'firstName'),
+  lastName: (val: any) => fieldTest(val, 'lastName'),
+  email: (val: any) => fieldTest(val, 'email'),
+  contact: (val: any) => fieldTest(val, 'contact'),
+  address: (val: any) => fieldTest(val, 'address')
+}
+</script>
 
 <template>
-  <div>
-    Page: Form
-  </div>
+  <VContainer>
+    <Header title="Create User" subtitle="Create a Nuew User Profile" />
+    <VForm @submit.prevent="handleSubmit">
+      <VContainer>
+        <VRow>
+          <VCol>
+            <VTextField v-model="formState.firstName" :rules="[fieldRules.firstName]" label="First name"
+              color="primary" />
+          </VCol>
+          <VCol>
+            <VTextField v-model="formState.lastName" :rules="[fieldRules.lastName]" label="Last name" color="primary" />
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol>
+            <VTextField v-model="formState.email" :rules="[fieldRules.email]" label="Email" color="primary" />
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol>
+            <VTextField v-model="formState.contact" :rules="[fieldRules.contact]" label="Contact" color="primary" />
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol>
+            <VTextField v-model="formState.address" :rules="[fieldRules.address]" label="Address" color="primary" />
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="1">
+            <VBtn type="submit" color="secondary">Submit</VBtn>
+          </VCol>
+          <VCol cols="1">
+            <VBtn type="reset" variant="outlined" color="neutral">Reset</VBtn>
+          </VCol>
+        </VRow>
+      </VContainer>
+    </VForm>
+  </VContainer>
 </template>
-
-<style scoped></style>
