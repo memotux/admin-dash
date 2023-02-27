b0a77292dca8b7264cb67dc6501fb87d625efc07
memoTux <romeo@mendezfuentes.net>
2023-02-27 09:42:32 -0600

    add faq page

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,9 +1,16 @@
-<script lang="ts" setup></script>
+<script setup lang="ts">
+const headData = {
+  title: 'FAQ',
+  description: 'Frequently Asked Questions Page'
+}
+useSeoMeta(headData)
+</script>
 
 <template>
-  <div>
-    Page: FAQ
-  </div>
+  <Header v-bind="headData" />
+  <VExpansionPanels color="greenAccent-800" variant="popout" class="my-4">
+    <VExpansionPanel v-for="i in 6" :key="'faq' + i" :title="`Item ${i}`"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+    </VExpansionPanel>
+  </VExpansionPanels>
 </template>
-
-<style scoped></style>
